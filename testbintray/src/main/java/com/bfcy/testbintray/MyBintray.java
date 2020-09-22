7ab78ade394fa02a9c322d8d7f4a303b91d8acab
Wu <banfengcanyue@163.com>
2020-09-22 10:21:07 +0800

    上传Bintray和jitpack代码提交

---
+++ testbintray/src/main/java/com/bfcy/testbintray/MyBintray.java
@@ -0,0 +1,11 @@
+package com.bfcy.testbintray;
+
+import android.util.Log;
+
+public class MyBintray {
+    public String test() {
+        String string = "Hello Bintray";
+        Log.i("tag", string);
+        return string;
+    }
+}
